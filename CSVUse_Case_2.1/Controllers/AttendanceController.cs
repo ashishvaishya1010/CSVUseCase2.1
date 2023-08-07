10dca4afb90903c893a5fd538edf5aba3ec7c44d
ashish.vaishya <ashish.vaishya@KELLGGNLPTP1116>
2023-08-07 10:26:30 +0530

    Commit

--- CSVUse_Case_2.1/Controllers/AttendanceController.cs
+++ CSVUse_Case_2.1/Controllers/AttendanceController.cs
@@ -7,7 +7,7 @@ using System.Net.Http;
 using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
-using Newtonsoft.Json;
+//using Newtonsoft.Json;
 
 namespace UpdatedAttendanceTask.Controllers
 {
